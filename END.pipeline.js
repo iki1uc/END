54df083d63e5f4e4eaab4c4d36155b4fbd46c6c8
iki1uc <entertrainer@web.de>
2026-07-23 03:59:09 +0200

    Create END.pipeline.js

---
+++ END.pipeline.js
@@ -0,0 +1,9 @@
+class END_PIPE {
+
+    fuse(un, mvmr, whirl, octa, real){
+        END.setInputs(un, mvmr, whirl, octa, real);
+        return END.compute();
+    }
+}
+
+window.END_PIPE = new END_PIPE();
